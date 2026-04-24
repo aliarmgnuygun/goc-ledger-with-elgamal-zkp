936db5a88c73f8dc4763451ecf90f8f2324ccf77
aliarmaganuygun <aliarmaganuygun@gmail.com>
2026-04-24 11:51:17 +0200

    Add initial project structure with Maven configuration and cryptographic classes

---
+++ src/main/java/com/goc/core/KeyPair.java
@@ -0,0 +1,13 @@
+package com.goc.core;
+
+import java.math.BigInteger;
+
+public class KeyPair {
+    public final BigInteger secretKey;
+    public final BigInteger publicKey;
+
+    public KeyPair(BigInteger secretKey, BigInteger publicKey) {
+        this.secretKey = secretKey;
+        this.publicKey = publicKey;
+    }
+}
