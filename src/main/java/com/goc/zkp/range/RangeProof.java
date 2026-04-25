b8439079dd383bbec35f5ad4d0f6adb1f462202b
aliarmaganuygun <aliarmaganuygun@gmail.com>
2026-04-26 01:46:38 +0200

    feat: refactor OrProof to use record type and update RangeProof to include bit proofs

--- src/main/java/com/goc/zkp/range/RangeProof.java
+++ src/main/java/com/goc/zkp/range/RangeProof.java
@@ -2,22 +2,27 @@ package com.goc.zkp.range;
 
 import com.goc.core.Ciphertext;
 import com.goc.zkp.Proof;
+import com.goc.zkp.range.bitdecomposition.OrProof;
 
 public class RangeProof implements Proof {
 
     private final Ciphertext[] encryptedBits;
+    private final OrProof[] bitProofs;
     private final Ciphertext encryptedValue;
     private final int bitLength;
 
     public RangeProof(Ciphertext[] encryptedBits,
+                      OrProof[] bitProofs,
                       Ciphertext encryptedValue,
                       int bitLength) {
         this.encryptedBits = encryptedBits;
+        this.bitProofs = bitProofs;
         this.encryptedValue = encryptedValue;
         this.bitLength = bitLength;
     }
 
     public Ciphertext[] getEncryptedBits() { return encryptedBits; }
+    public OrProof[] getBitProofs() { return bitProofs; }
     public Ciphertext getEncryptedValue()  { return encryptedValue; }
     public int getBitLength()              { return bitLength; }
 }
--- src/main/java/com/goc/zkp/range/bitdecomposition/OrProof.java
+++ src/main/java/com/goc/zkp/range/bitdecomposition/OrProof.java
@@ -4,35 +4,14 @@ import com.goc.core.Ciphertext;
 
 import java.math.BigInteger;
 
-class OrProof {
-
-    final Ciphertext encryptedBits;
-
-    final BigInteger commitmentA0;
-    final BigInteger commitmentD0;
-
-    final BigInteger commitmentA1;
-    final BigInteger commitmentD1;
-
-    final BigInteger challengeE0;
-    final BigInteger challengeE1;
-
-    final BigInteger responseZ0;
-    final BigInteger responseZ1;
-
-    OrProof(Ciphertext encryptedBits,
-            BigInteger commitmentA0, BigInteger commitmentD0,
-            BigInteger commitmentA1, BigInteger commitmentD1,
-            BigInteger challengeE0, BigInteger challengeE1,
-            BigInteger responseZ0, BigInteger responseZ1) {
-        this.encryptedBits = encryptedBits;
-        this.commitmentA0 = commitmentA0;
-        this.commitmentD0 = commitmentD0;
-        this.commitmentA1 = commitmentA1;
-        this.commitmentD1 = commitmentD1;
-        this.challengeE0 = challengeE0;
-        this.challengeE1 = challengeE1;
-        this.responseZ0 = responseZ0;
-        this.responseZ1 = responseZ1;
-    }
-}
+public record OrProof(
+        Ciphertext encryptedBit,
+        BigInteger commitmentA0,
+        BigInteger commitmentD0,
+        BigInteger commitmentA1,
+        BigInteger commitmentD1,
+        BigInteger challengeE0,
+        BigInteger challengeE1,
+        BigInteger responseZ0,
+        BigInteger responseZ1
+) {}
